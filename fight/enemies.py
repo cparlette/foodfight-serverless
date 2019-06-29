a5d14c9567472f2fc608a911ce006f8c6b48063e
cparlette <cparlette@gmail.com>
2019-06-28 22:11:28 -0400

    Create initial enemies dict

---
+++ fight/enemies.py
@@ -0,0 +1,52 @@
+enemies = {
+	1: [
+		{'name': 'Taco-Show Host', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Peas Treaty', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Flaming Fruit', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Fish Friar', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Taco Bellboy', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Short Order Cook', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Tall Order Cook', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5},
+		{'name': 'Small Fry', 'xp': 5, 'health': 5, 'money': 5, 'damage': 5}
+	],
+	2: [
+		{'name': 'Galloping Gourmet', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Cookie Monster', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Crabby Critic', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Small Fry', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Burger King', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Dairy Queen', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Pullet Surprise', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10},
+		{'name': 'Iceburger', 'xp': 10, 'health': 10, 'money': 10, 'damage': 10}
+	],
+	3: [
+		{'name': 'Never On Sundae', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Ice Cream Clone', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Tom Mato', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Raisin Hell', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Chip Monk', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Burrito Bandito', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Bunch Of Baloney', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20},
+		{'name': 'Jerrys Squids', 'xp': 20, 'health': 20, 'money': 20, 'damage': 20}
+	],
+	4: [
+		{'name': 'Boysenberry', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Girlsenberry', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Chuck Berry', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Gingerbread Man', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Cocoa Nut', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Spambo', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Emberrysment', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40},
+		{'name': 'Apple IIc', 'xp': 40, 'health': 40, 'money': 40, 'damage': 40}
+	],
+	5: [
+		{'name': 'Annette Funijello', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Sloppy Joe', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Grape Ape', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Pete Za', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Pepe Roni', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Hamburger Helper', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Pillsbury Doughboy', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80},
+		{'name': 'Rosanna Bananadanna', 'xp': 80, 'health': 80, 'money': 80, 'damage': 80}
+	],
+}
